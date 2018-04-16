079afb4da153848354c43c675d1697cc967f0982
Kyle Coberly <kyle.coberly@gmail.com>
2018-04-15 22:39:27 -0600

    Updating S3 example

--- src/components/apis/s3-uploads.vue
+++ src/components/apis/s3-uploads.vue
@@ -1,13 +1,5 @@
 /* waks:start=HTML Form=start */
-Before you start, you need to setup your bucket on S3.
-
-1. Log into the AWS console and go to S3
-2. Create a bucket. Give it a name, and set the permissions to "Grant public read access to this bucket." Use the default settings for everything else.
-3. Click on your new bucket, and take note of the `region` value in the URL (eg. `us-west-2`). Add this to your server environment.
-4. Next, go to the IAM service on AWS, then go to "Users", then "Add User."
-5. Add a descriptive name for the user, choose the "Programmatic Access" option, and go to the next step.
-6. Copy the "Access key ID" and the "Secret access key" to your environment - it's the last opportunity to see them, so don't wait.
-7. Click on "Bucket Policy". We're going to add the ARN for your user to the policy for this bucket, which gives them access.
+The form uses a `file` upload type. Note that these look different from browser to browser, and can be hard to style directly. You can use the `File` API for more fine-grained control.
 <!-- waks:example -->
 <template>
 <div class="s3">
@@ -17,7 +9,6 @@ Before you start, you need to setup your bucket on S3.
 
         <input type="submit" value="Upload File" />
 
-        <div id="error"></div>
         <div id="message"></div>
     </form>
 </div>
@@ -29,23 +20,34 @@ Before you start, you need to setup your bucket on S3.
         mounted(){
 /* waks:start=Client-Side Code=start
 waks:example */
+// Store the message element
+const $message = document.querySelector("#message");
+
+// Attach a listener to the submit event for the form
 document.querySelector("form").addEventListener("submit", event => {
+    // Prevent the browser from handling the form submission directly
     event.preventDefault();
-    const formData = new FormData(event.target);
-    fetch("https://web-app-kitchen-sink-api.herokuapp.com/apis/s3-uploads/upload", {
+
+    // Upload endpoint on the server
+    const URL = "https://web-app-kitchen-sink-api.herokuapp.com/apis/s3-uploads/upload";
+    fetch(URL, {
         method: "POST",
-        body: formData,
-        "Content-Type": "multipart/form-data"
+        body: FormData(event.target), // The body will be the uploaded file
+        headers: {
+            // Important! Your server can only process files with this type
+            "Content-Type": "multipart/form-data"
+        }
     }).then(response => response.json())
     .then(({data}) => {
-        document.querySelector("#message").innerHTML = `
+        // Show the URL of the uploaded file
+        $message.innerHTML = `
             <p>File was uploaded to: <a href="${data}">${data}<a></p>
         `;
     }).catch(error => {
-        document.querySelector("#error").textContent = error.message;
+        // If there was a problem, show the error message
+        $message.textContent = error.message;
     });
 });
-
 /* waks:end */
         }
     };
@@ -55,7 +57,7 @@ document.querySelector("form").addEventListener("submit", event => {
 @import "~@/styles/_colors";
 
 /* waks:start=Styles=start
-The style on the form is basic form styling. The only exception is the element that's created by Stripe, which has the `.StripeElement` class and a few helper classes as seen below.
+Basic form styling.
 waks:example */
 .s3 {
     padding: 1rem;
@@ -70,9 +72,6 @@ waks:example */
             border: none;
             box-shadow: 0px 0px 4px $light-grey;
             border-radius: 4px;
-            &::placeholder {
-                color: $light-grey;
-            }
             &[type=submit] {
                 margin-top: 1rem;
                 font-size: 1rem;
