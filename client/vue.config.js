48b78d3d861c0a47e10ff6db72dccc7b16cd09a2
Kyle Coberly <kyle.coberly@gmail.com>
2018-06-20 15:41:57 -0600

    Fixing address bar coloring

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,2 +1,15 @@
+const manifestJSON = require("./public/manifest.json");
+
+const pwaArgs = {
+    themeColor: manifestJSON.theme_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color
+};
+
 module.exports = {
-}
+    chainWebpack: config => {
+        config.plugin("pwa").tap(args => {
+            return [pwaArgs];
+        });
+    }
+};
